8420b549720b29183375bcd40f4d38cdcd592398
Pepe Barbe <dev@antropoide.net>
2016-06-12 01:21:14 -0500

    Refactor Account class to make thread safe

--- src/main/java/net/antropoide/Account.java
+++ src/main/java/net/antropoide/Account.java
@@ -1,33 +1,113 @@
+package net.antropoide;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * The Account class represents an account and it's current fund state; it allows adding or withdrawing funds
+ * from the accout and transferring funds between accounts.
+ *
+ * @author Pepe Barbe
+ */
 public class Account {
     private long id;
-    private int availableFunds;
+    private AtomicInteger availableFunds;
     // Getters and setters for the above fields.
 
+    /**
+     * Constructor, intializes account with balance 0
+     *
+     * @param id    account id
+     */
     public Account(long id) {
         this.id = id;
-        this.availableFunds = 0;
+        this.availableFunds = new AtomicInteger();
     }
 
-    public Account(long id, int availableFunds) {
-        this.id = id;
-        this.availableFunds = availableFunds;
-    }
-
-    public int getAvailableFunds() {
-        return availableFunds;
-    }
-
-    public void setAvailableFunds(int availableFunds) {
-        this.availableFunds = availableFunds;
-    }
-
-    public static void transferFunds(Account source, Account destination, int amount) {
-        if (source.getAvailableFunds() >= amount) {
-            source.setAvailableFunds(source.getAvailableFunds() - amount);
-            destination.setAvailableFunds(destination.getAvailableFunds() + amount);
-        } else {
-            throw new IllegalArgumentException("Insufficient funds in source: " + source);
-    
+    /**
+     * Constructor, intializes account with an arbitrary balance, which can't be negative
+     *
+     * @param   id    account id
+     * @param   funds starting balance of account, can't be negative
+     * @throws  IllegalArgumentException
+     */
+    public Account(long id, int funds) {
+        if (funds < 0) {
+            throw new IllegalArgumentException("`funds` can not be negative");
         }
+
+        this.id = id;
+        this.availableFunds = new AtomicInteger(funds);
+    }
+
+
+    /**
+     * Return account id
+     *
+     * @return account id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * Return the funds in the account
+     *
+     * @return funds
+     */
+    public int getAvailableFunds() {
+        return availableFunds.get();
+    }
+
+    /**
+     * Add amount to current funds in the account and return the total amount of funds in the account
+     *
+     * @param amount    increase funds by amount, can't be negative
+     * @return new total funds in the account after adding
+     * @throws IllegalArgumentException
+     */
+    public int depositAndGetFunds(int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("`amount` can not be negative");
+        }
+
+        return availableFunds.addAndGet(amount);
+    }
+
+    /**
+     * Remove amount from the account's funds, throws exception if the account does not hold sufficient funds
+     *
+     * @param amount    funds to remove from account
+     * @return new total funds in the account after withdrawing
+     * @throws IllegalArgumentException
+     */
+    public int withdrawAndGetFunds(int amount) {
+        int currentFunds, newFunds;
+
+        do {
+            currentFunds = availableFunds.get();
+
+            if (amount > currentFunds) {
+                throw new IllegalArgumentException("Insufficient funds:" + this);
+            }
+
+            newFunds = currentFunds - amount;
+        } while (!availableFunds.compareAndSet(currentFunds, newFunds));
+
+        return availableFunds.get();
+    } 
+
+
+    /**
+     * Transfer amount from the source account into the destination account, throws exception if account does
+     * not hold sufficient funds
+     *
+     * @param source      account to withdraw amount from
+     * @param destination account to add amount to
+     * @param amount      amount to transfer
+     * @throws IllegalArgumentException  
+     */ 
+    public static void transferFunds(Account source, Account destination, int amount) {
+        source.withdrawAndGetFunds(amount);
+        destination.depositAndGetFunds(amount);
     }
 }
--- src/test/groovy/net/antropoide/AccountTest.groovy
+++ src/test/groovy/net/antropoide/AccountTest.groovy
@@ -1,6 +1,106 @@
+package net.antropoide;
+
 import spock.lang.Specification
 
 class AccountTest extends Specification {
+    static long accountId = 12345;
 
+    def getNewAccount() {
+        return new Account(accountId);
+    }
+
+    def "instantiate Account"() {
+        when: 'create class'
+        def account = new Account(0)
+
+        then:
+        account.id == 0
+        account.availableFunds == 0
+
+        when: 'create class with funds'
+        account = new Account(1, 1)
+
+        then:
+        account.id == 1
+        account.availableFunds == 1
+    }
+
+    def "deposit funds"() {
+        given: 'account'
+        def account = this.newAccount
+
+        and: 'funds to deposit'
+        def deposit = 54321;
+
+        expect: 'current funds at 0'
+        account.availableFunds == 0
+
+        when: 'do deposit'
+        def newFunds = account.depositAndGetFunds(deposit)
+
+        then:
+        newFunds == deposit
+        newFunds == account.availableFunds
+    }
+
+    def "withdraw funds"() {
+        given: 'account'
+        def account = this.newAccount
+
+        and: 'funds available'
+        def deposit = 100;
+        account.depositAndGetFunds(deposit)
+
+        and: 'funds to withdraw'
+        def withdraw = 90 
+
+        expect: 'account to have funds'
+        account.availableFunds == deposit
+   
+        when: 'withdraw funds'
+        def newFunds = account.withdrawAndGetFunds(withdraw)
+
+        then:
+        newFunds == deposit - withdraw
+        newFunds == account.availableFunds
+
+        when: 'withdraw too much'
+        account.withdrawAndGetFunds(withdraw)
+
+        then:
+        thrown IllegalArgumentException
+    }
+
+    def "transfer funds"() {
+        given: 'account with funds'
+        def initialFunds = 100
+        def account_1 = new Account(1);
+        account_1.depositAndGetFunds(initialFunds);
+
+        and: 'empty account'
+        def account_2 = new Account(2);
+
+        and: 'amount to transfer'
+        def transfer = 90
+
+        expect:
+        account_1.id == 1
+        account_1.availableFunds == initialFunds
+        account_2.id == 2
+        account_2.availableFunds == 0
+
+        when: 'transfer part of the funds'
+        Account.transferFunds(account_1, account_2, transfer)
+
+        then:
+        account_1.availableFunds == initialFunds - transfer
+        account_2.availableFunds == transfer
+
+        when: 'transfer too much'
+        Account.transferFunds(account_1, account_2, transfer)
+
+        then:
+        thrown IllegalArgumentException
+    }
 }
 
